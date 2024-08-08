63300dc1f42935bfc922d8e9eff6c3fe16f56ff5
staff-gymnast <staff-gymnast@outlook.com>
2024-08-08 18:25:04 +0000

    Update main.py

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install -y libcairo2-dev libpango1.0-dev
-sudo apt-get install ffmpeg
-sudo apt-get install texlive-full
+sudo apt-get install ffmpeg -y
+sudo apt-get install texlive-full -y
--- main.py
+++ main.py
@@ -1,25 +1,58 @@
 from manim import *
 
-class MyFirstAnimation(Scene):
+class CalculusVideo(Scene):
     def construct(self):
-        # Create a text object
-        text = Text("Hello, Manim!")
-        
-        # Create a mathematical equation
-        equation = MathTex(r"E=mc^2")
-        
-        # Add the text to the scene
-        self.play(Write(text))
-        
-        # Wait for 2 seconds
-        self.wait(2)
-        
-        # Transform the text into the equation
-        self.play(Transform(text, equation))
-        
-        # Wait for 2 seconds
-        self.wait(2)
+        self.play_calculus_intro()
+        self.play_limits()
+        self.play_derivative()
+        self.play_integration()
 
-if __name__ == "__main__":
-    config.background_color = WHITE
-    MyFirstAnimation().render()
+    def play_calculus_intro(self):
+        title = Text("Introduction to Calculus")
+        self.play(Write(title))
+        self.wait(2)
+        self.play(FadeOut(title))
+
+    def play_limits(self):
+        title = Text("Limits")
+        definition = Text("The limit of a function is the value that the function approaches as the input approaches some value.")
+        self.play(Write(title))
+        self.wait(2)
+        self.play(Transform(title, definition))
+        self.wait(2)
+        self.play(FadeOut(title))
+
+    def play_derivative(self):
+        axes = Axes(
+            x_range=[-3, 3, 1],
+            y_range=[-1, 8, 1],
+            axis_config={"color": BLUE},
+        )
+
+        graph = axes.plot(lambda x: x**2, color=WHITE)
+        tangent_line = TangentLine(
+            graph, alpha=0.5, length=5, color=RED
+        )
+
+        self.play(Create(axes))
+        self.play(Create(graph))
+        self.wait(1)
+        self.play(Create(tangent_line))
+        self.wait(2)
+        self.play(FadeOut(axes), FadeOut(graph), FadeOut(tangent_line))
+
+    def play_integration(self):
+        axes = Axes(
+            x_range=[0, 5, 1],
+            y_range=[0, 10, 1],
+            axis_config={"color": BLUE},
+        )
+
+        graph = axes.plot(lambda x: x**2, color=WHITE)
+        area = axes.get_area(graph, x_range=[1, 3], color=[GREEN, BLUE])
+
+        self.play(Create(axes))
+        self.play(Create(graph))
+        self.wait(1)
+        self.play(Create(area))
+        self.wait(2)
